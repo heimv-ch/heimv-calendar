3dae1c8bfd437abe805c449c0d2cbbf2f3bd9ad1
Diego Steiner <diego.steiner@u041.ch>
2026-06-01 09:07:36 +0000

    fix: esm support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import packageJson from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -15,18 +14,11 @@ export default defineConfig({
   build: {
     lib: {
       entry: "src/index.ts",
-      name: packageJson.name,
-      formats: ["es", "umd"],
-      fileName: (format) => `index.${format}.js`,
+      formats: ["es"],
+      fileName: () => "index.js",
     },
     rollupOptions: {
       external: ["react", "react-dom", "date-fns", "@floating-ui/react"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
     },
     cssCodeSplit: true,
     emptyOutDir: true,
